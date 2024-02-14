c034a38e61675a088e878263ea4c1d68415d5cf2
Koul <dongik0122@gmail.com>
2024-02-15 03:17:57 +0900

    개발 환경 설정
    pug, express JS를 활용하여 개발 환경을 설정하였다.
    추가적으로 더 나은 UI를 위해 MVP CSS를 도입하였다.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import * as path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log('Listening');
+app.listen(3000, handleListen);
